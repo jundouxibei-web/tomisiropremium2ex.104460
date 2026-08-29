031f15d70608ee3b2f00d59b976f0c58e2a0681d
jundouxibei-web <jundouxibei@gmail.com>
2026-08-29 23:00:47 +0900

    script.js

--- script.js
+++ script.js
@@ -8,23 +8,23 @@ const answer3 = "かいとよシロー";
  if (button0) {
  button0.addEventListener("click", () => {
 
-  localStorage.setItem("score", 1);
+  localStorage.setItem("score2ex", 1);
   location.href = 'index2.html';
  });
 }
 
 
-let score = Number(localStorage.getItem("score")) || 0;
+let score2ex = Number(localStorage.getItem("score2ex")) || 0;
 
 const button11 = document.getElementById("checktoi11");
  if (button11) {
  button11.addEventListener("click", () => {
   const input = document.getElementById("toyo11").value;
   if (input === answer1) {
-    score += 33; 
-    document.getElementById("score").textContent = score;
+    score2ex += 33; 
+    document.getElementById("score2ex").textContent = score2ex;
    console.log('+10');
-    localStorage.setItem("score", score); // ← 保存
+    localStorage.setItem("score2ex", score2ex); // ← 保存
   } else {
    console.log('0');
   }
@@ -38,10 +38,10 @@ const button12 = document.getElementById("checktoi12");
  button12.addEventListener("click", () => {
   const input = document.getElementById("toyo12").value;
   if (input === answer2) {
-    score += 33; 
-    document.getElementById("score").textContent = score;
+    score2ex += 33; 
+    document.getElementById("score2ex").textContent = score2ex;
    console.log('+10');
-    localStorage.setItem("score", score); // ← 保存
+    localStorage.setItem("score2ex", score2ex); // ← 保存
   } else {
    console.log('0');
   }
@@ -54,10 +54,10 @@ const button13 = document.getElementById("checktoi13");
  button13.addEventListener("click", () => {
   const input = document.getElementById("toyo13").value;
   if (input === answer3) {
-    score += 33; 
-    document.getElementById("score").textContent = score;
+    score2ex += 33; 
+    document.getElementById("score2ex").textContent = score2ex;
    console.log('+10');
-    localStorage.setItem("score", score); // ← 保存
+    localStorage.setItem("score2ex", score2ex); // ← 保存
   } else {
    console.log('0');
   }
@@ -70,29 +70,29 @@ function check11() {
   document.querySelector('.toi').style.display = 'none';
   document.querySelector('.kaito').style.display = 'block';
 
-if (score >=1){
-    if(score<=34){
+if (score2ex >=1){
+    if(score2ex<=34){
      document.getElementById("itimon").textContent = "今回君が正解したのは一問だ。EXは答え合わせをしないからリンクを保存して何度でも挑戦してみよう！";
     }
 }
 
-if (score >=35){
-    if(score<=67){
+if (score2ex >=35){
+    if(score2ex<=67){
      document.getElementById("nimon").textContent = "惜しい！今回君は2問正解した。あと一問の場所は見当がついているのかな？もう一度挑戦しよう！";
     }
 }
 
-if (score >=68){
-    if(score<=100){
+if (score2ex >=68){
+    if(score2ex<=100){
         document.getElementById('manten').textContent = "おめでとう！ついに全問正解だ！ここまでたどり着くのにどれほどの時間がかかったかと思うと恐ろしいよ。第三回目は結果掲示板の工夫などに取り組んでいくよ！";
     }
 }
 
 }
 
-document.getElementById("result").textContent =  score + "点";
+document.getElementById("result").textContent =  score2ex + "点";
 
-const share_title = `とよシローなにシロークイズ２EX${score}点でした。`;
+const share_title = `とよシローなにシロークイズ２EX${score2ex}点でした。`;
   const share_twitter = document.getElementById("js-share-twitter");
   share_twitter.setAttribute(
       "href",
